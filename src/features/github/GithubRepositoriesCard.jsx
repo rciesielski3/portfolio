2f671dcd6c41fa99ac944aa9042094682ce79c4b
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-07-31 15:59:09 +0200

    fix: add mobile view, add title style for git

--- src/features/github/GithubRepositoriesCard.jsx
+++ src/features/github/GithubRepositoriesCard.jsx
@@ -5,7 +5,7 @@ const GithubCard = ({ repo }) => {
     <>
       <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
         <div className="repo-box">
-          {repo.name}
+          <p className="repo-title">{repo.name}</p>
           <p>{repo.description || "No description provided."}</p>
         </div>
       </a>
