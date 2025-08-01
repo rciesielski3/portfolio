920df0bf8fee0014da389691dbc3ada447acd323
r.ciesielski3 <r.ciesielski3@gmail.com>
2025-08-01 07:07:09 +0200

    feat: add context to main page for a im

--- src/components/typing/TypingEffect.jsx
+++ src/components/typing/TypingEffect.jsx
@@ -1,44 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./TypingEffect.css";
+
+import { DataContext } from "../../context/DataContext";
 
 const TypingEffect = () => {
   const [currentWord, setCurrentWord] = useState("");
   const [isDeleting, setIsDeleting] = useState(false);
   const [loopIndex, setLoopIndex] = useState(0);
   const [typingSpeed, setTypingSpeed] = useState(150);
-  const [words, setWords] = useState([]);
+  const { iAm } = useContext(DataContext);
   const staticText = "I'm";
 
-  // Fetch words from public/data/iAm.json
-  useEffect(() => {
-    const fetchWords = async () => {
-      try {
-        const response = await fetch("/data/iAm.json");
-        if (!response.ok) throw new Error("Network response was not ok");
-        const data = await response.json();
-        setWords(data);
-      } catch (error) {
-        console.error("Failed to fetch words from iAm.json:", error);
-        setWords(["Quality Assurance Expert"]);
-      }
-    };
-
-    fetchWords();
-  }, []);
-
   React.useEffect(() => {
-    if (words.length === 0) return;
+    if (iAm.length === 0) return;
 
-    const fullWord = words[loopIndex % words.length];
+    const fullWord = iAm[loopIndex % iAm.length];
 
     const updateWord = () => {
-      setCurrentWord((prev) => {
-        if (isDeleting) {
-          return fullWord.substring(0, prev.length - 1);
-        } else {
-          return fullWord.substring(0, prev.length + 1);
-        }
-      });
+      setCurrentWord((prev) =>
+        isDeleting
+          ? fullWord.substring(0, prev.length - 1)
+          : fullWord.substring(0, prev.length + 1)
+      );
 
       if (!isDeleting && currentWord === fullWord) {
         setTimeout(() => setIsDeleting(true), 1000);
@@ -52,7 +35,7 @@ const TypingEffect = () => {
 
     const timeout = setTimeout(updateWord, typingSpeed);
     return () => clearTimeout(timeout);
-  }, [currentWord, isDeleting, loopIndex, typingSpeed, words]);
+  }, [currentWord, isDeleting, loopIndex, typingSpeed, iAm]);
 
   return (
     <>
